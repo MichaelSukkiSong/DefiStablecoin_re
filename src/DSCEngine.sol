8c6a9dfe3b9cf408682b31291e7ce25c79703b08
MichaelSukkiSong <sa77irak@gmail.com>
2024-10-31 17:17:54 +0900

    add: deposit collateral function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -3,12 +3,16 @@
 pragma solidity ^0.8.18;
 
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract DSCEngine {
     ///////////////
     // errors    //
     ///////////////
     error DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+    error DSCEngine__NeedsMoreThanZero();
+    error DSCEngine__NotAllowedToken();
+    error DSCEngine__TransferFaileD();
 
     ///////////////
     // Type      //
@@ -20,16 +24,31 @@ contract DSCEngine {
 
     mapping(address token => address priceFeed) private s_priceFeeds;
     address[] private s_collateralTokens;
+    mapping(address user => mapping(address token => uint256)) private s_collateralDeposited;
 
     DecentralizedStableCoin private immutable i_dsc;
 
     ///////////////
     // events    //
     ///////////////
+    event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
 
     ///////////////
     // modifiers //
     ///////////////
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert DSCEngine__NeedsMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DSCEngine__NotAllowedToken();
+        }
+        _;
+    }
 
     ///////////////
     // Functions //
@@ -61,6 +80,19 @@ contract DSCEngine {
     ///////////////
     // public //
     ///////////////
+
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
+        public
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
+        bool success = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountCollateral);
+        if (!success) {
+            revert DSCEngine__TransferFaileD();
+        }
+    }
 
     ///////////////
     // internal //
