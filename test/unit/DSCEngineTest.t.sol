6a0ba28d87446f83464343da727af44d8cdf38d4
MichaelSukkiSong <sa77irak@gmail.com>
2024-11-06 17:27:17 +0900

    add: depositCollateral tests

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -21,6 +21,7 @@ contract DSCEngineTest is Test {
 
     address public USER = makeAddr("user");
     uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
 
     function setUp() external {
         deployer = new DeployDSC();
@@ -65,6 +66,49 @@ contract DSCEngineTest is Test {
     /////////////////////////////
     // depositCollateral Tests //
     /////////////////////////////
+
+    function test_RevertsIfCollateralZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
+
+    function test_RevertsWithUnapprovedCollateral() public {
+        ERC20Mock ranToken = new ERC20Mock("RAN", "RAN", USER, AMOUNT_COLLATERAL);
+        vm.startPrank(USER);
+        vm.expectRevert(DSCEngine.DSCEngine__NotAllowedToken.selector);
+        dsce.depositCollateral(address(ranToken), AMOUNT_COLLATERAL);
+        vm.stopPrank();
+    }
+
+    modifier depositedCollateral() {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+        dsce.depositCollateral(weth, AMOUNT_COLLATERAL);
+        vm.stopPrank();
+        _;
+    }
+
+    function test_CanDepositedCollateralAndGetAccountInfo() public depositedCollateral {
+        (uint256 totalDscMinted, uint256 collateralValueInUsd) = dsce.getAccountInformation(USER);
+
+        uint256 expectedTotalDscMinted = 0;
+        uint256 expectedDepositAmount = dsce.getTokenAmountFromUsd(weth, collateralValueInUsd);
+
+        assertEq(totalDscMinted, expectedTotalDscMinted);
+        assertEq(AMOUNT_COLLATERAL, expectedDepositAmount);
+    }
+
+    function test_DataStructureIsProperlyUpdated() public depositedCollateral {}
+
+    function test_EmitsCollateralDepositedEvent() public depositedCollateral {}
+
+    function test_TokenTransferIsCorrect() public depositedCollateral {}
+
+    function test_RevertsIfTokenTransferFails() public depositedCollateral {}
 
     ////////////////////////////
     // redeemCollateral Tests //
