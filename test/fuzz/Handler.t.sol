4299a696a1fd50ebfb9ca00f4f0c373275d6273d
MichaelSukkiSong <sa77irak@gmail.com>
2024-11-23 20:12:37 +0900

    add: added handler for invariant test, add: test depositCollateral func in handler

---
+++ test/fuzz/Handler.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+
+contract Handler is Test {
+    DecentralizedStableCoin dsc;
+    DSCEngine dsce;
+
+    ERC20Mock weth;
+    ERC20Mock wbtc;
+
+    uint256 constant MAX_DEPOSIT_SIZE = type(uint96).max;
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
+        dsce = _dscEngine;
+        dsc = _dsc;
+
+        address[] memory collateralTokens = dsce.getCollateralTokens();
+        weth = ERC20Mock(collateralTokens[0]);
+        wbtc = ERC20Mock(collateralTokens[1]);
+    }
+
+    function depositCollateral(uint256 tokenCollateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getTokenCollateralFromSeed(tokenCollateralSeed);
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_SIZE);
+
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender, amountCollateral);
+        collateral.approve(address(dsce), amountCollateral);
+        dsce.depositCollateral(address(collateral), amountCollateral);
+        vm.stopPrank();
+    }
+
+    function _getTokenCollateralFromSeed(uint256 tokenCollateralSeed) public view returns (ERC20Mock) {
+        if (tokenCollateralSeed % 2 == 0) {
+            return weth;
+        }
+        return wbtc;
+    }
+}
