0d4686845d34a480246dc41ed7291de982bcea20
MichaelSukkiSong <sa77irak@gmail.com>
2024-11-26 16:41:42 +0900

    add: updateCollateralPrice handler

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -6,6 +6,7 @@ import {Test} from "forge-std/Test.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+import {MockV3Aggregator} from "@chainlink/contracts/src/v0.8/tests/MockV3Aggregator.sol";
 
 contract Handler is Test {
     DecentralizedStableCoin dsc;
@@ -15,6 +16,7 @@ contract Handler is Test {
     ERC20Mock wbtc;
 
     address[] public usersWithCollateralDeposited;
+    MockV3Aggregator public ethUsdPriceFeed;
 
     uint256 constant MAX_DEPOSIT_SIZE = type(uint96).max;
 
@@ -25,6 +27,8 @@ contract Handler is Test {
         address[] memory collateralTokens = dsce.getCollateralTokens();
         weth = ERC20Mock(collateralTokens[0]);
         wbtc = ERC20Mock(collateralTokens[1]);
+
+        ethUsdPriceFeed = MockV3Aggregator(dsce.getPriceFeedAddress(address(weth)));
     }
 
     function depositCollateral(uint256 tokenCollateralSeed, uint256 amountCollateral) public {
@@ -73,6 +77,15 @@ contract Handler is Test {
         dsce.mintDsc(amoutDscToMint);
         vm.stopPrank();
     }
+
+    // function updateCollateralPrice(uint96 newPrice) public {
+    //     int256 newPriceInt = int256(uint256(newPrice));
+    //     ethUsdPriceFeed.updateAnswer(newPriceInt);
+    // }
+
+    //////////////////////
+    // Helper functions //
+    //////////////////////
 
     function _getTokenCollateralFromSeed(uint256 tokenCollateralSeed) public view returns (ERC20Mock) {
         if (tokenCollateralSeed % 2 == 0) {
